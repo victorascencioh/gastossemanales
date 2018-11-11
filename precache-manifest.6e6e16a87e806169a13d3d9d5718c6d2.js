a1ee043c8e9ac1ec2e78b748df4c3dd4e6a5bd65
victorascencioh <victorascencioh@gmail.com>
2018-11-11 17:53:09 +0100

    Updates

---
+++ precache-manifest.6e6e16a87e806169a13d3d9d5718c6d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed8fd42063c65933e3d",
+    "url": "/gastosemanal/static/js/runtime~main.fed8fd42.js"
+  },
+  {
+    "revision": "d0011a271c3350c85bc2",
+    "url": "/gastosemanal/static/js/main.d0011a27.chunk.js"
+  },
+  {
+    "revision": "6ee94f985ede9c95170e",
+    "url": "/gastosemanal/static/js/1.6ee94f98.chunk.js"
+  },
+  {
+    "revision": "d0011a271c3350c85bc2",
+    "url": "/gastosemanal/static/css/main.51b348f9.chunk.css"
+  },
+  {
+    "revision": "051eb5d8af3ff39de8d3a1f2481847c9",
+    "url": "/gastosemanal/index.html"
+  }
+];
